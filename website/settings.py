72456e99d6b2a6b826f539ec39457c1341ea6d0b
Patryk Karbowy <patryk.karbowy11@gmail.com>
2022-08-10 18:00:53 +0200

    Preparing for password reset

--- website/settings.py
+++ website/settings.py
@@ -144,3 +144,12 @@ LOGIN_URL = '/login'
 
 LOGIN_REDIRECT_URL = "/home"
 LOGOUT_REDIRECT_URL = "/login"
+
+# SMTP CONFIGURATION
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
