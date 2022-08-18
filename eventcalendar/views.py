b6fec47f66f6a1fb062d019770549843431f3e89
Patryk Karbowy <patryk.karbowy11@gmail.com>
2022-08-18 19:41:57 +0200

    Updating calendar

--- eventcalendar/views.py
+++ eventcalendar/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
+from .models import Lesson
+from django.views import generic
+from .utils import Calendar
+from django.utils.safestring import mark_safe
 
-def index(request):
-    return HttpResponse("Hello")
+class CalendarView(generic.ListView):
+    model = Lesson
+    template_name = 'calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return datetime(year, month, day=1)
+    return datetime.today()
+    
 
 
